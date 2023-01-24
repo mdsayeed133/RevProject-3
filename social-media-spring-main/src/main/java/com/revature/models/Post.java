c9b6ee07ed13632975c45c4ab5f8c6de7872f3e0
Zackery Perski <zackeryperski@gmail.com>
2023-01-24 12:30:26 -0500

    Updated DTOs and Models to utilize less Lombok.

--- social-media-spring-main/src/main/java/com/revature/models/Post.java
+++ social-media-spring-main/src/main/java/com/revature/models/Post.java
@@ -8,10 +8,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Table(name = "posts")
 public class Post {
 
@@ -26,4 +24,72 @@ public class Post {
 	private User author;
 
 	private PostType postType;
+
+	public Post() {
+	}
+
+	public Post(String text, String imageUrl, List<Post> comments, User author, PostType postType) {
+		this.text = text;
+		this.imageUrl = imageUrl;
+		this.comments = comments;
+		this.author = author;
+		this.postType = postType;
+	}
+
+	public Post(int id, String text, String imageUrl, List<Post> comments, User author, PostType postType) {
+		this.id = id;
+		this.text = text;
+		this.imageUrl = imageUrl;
+		this.comments = comments;
+		this.author = author;
+		this.postType = postType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public String getImageUrl() {
+		return imageUrl;
+	}
+
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+
+	public List<Post> getComments() {
+		return comments;
+	}
+
+	public void setComments(List<Post> comments) {
+		this.comments = comments;
+	}
+
+	public User getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+
+	public PostType getPostType() {
+		return postType;
+	}
+
+	public void setPostType(PostType postType) {
+		this.postType = postType;
+	}
 }
--- social-media-spring-main/src/main/java/com/revature/models/User.java
+++ social-media-spring-main/src/main/java/com/revature/models/User.java
@@ -6,10 +6,8 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Data
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Table(name = "users")
 public class User {
 
@@ -20,4 +18,67 @@ public class User {
     private String password;
     private String firstName;
     private String lastName;
+
+    /*        User created = new User(0,
+                registerRequest.getEmail(),
+                registerRequest.getPassword(),
+                registerRequest.getFirstName(),
+                registerRequest.getLastName());*/
+    public User() {
+    }
+
+    public User(int id, String email, String password, String firstName, String lastName) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public User(String email, String password, String firstName, String lastName) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
