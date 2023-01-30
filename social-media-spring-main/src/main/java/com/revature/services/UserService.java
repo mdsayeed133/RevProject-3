3ae0331c26218ad2fc379be454c75fb7b1b356e8
Mason <Mason@DESKTOP-NHDIDLB>
2023-01-30 15:39:26 -0500

    First Push

--- social-media-spring-main/src/main/java/com/revature/services/UserService.java
+++ social-media-spring-main/src/main/java/com/revature/services/UserService.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import com.revature.models.User;
 import com.revature.repositories.UserRepository;
+import com.revature.models.User;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -18,6 +19,20 @@ public class UserService {
     public Optional<User> findByCredentials(String email, String password) {
         return userRepository.findByEmailAndPassword(email, password);
     }
+
+    public Optional<User> getUserById(int id){
+        return userRepository.findById(id);
+    }
+    public boolean updatePassword(int userId, String newPassword){
+        User user = userRepository.findById(userId).orElse(null);
+        if(user == null){
+            return false;
+        }
+        user.setPassword(newPassword);
+        userRepository.save(user);
+        return true;
+
+    };
 
     public User save(User user) {
         return userRepository.save(user);
