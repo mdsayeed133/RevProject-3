4db49215e441006234881610d0d8eb7776fb7a13
Mohammed Sayeed Islam <120868662+mdsayeed133@users.noreply.github.com>
2023-01-30 09:03:33 -0800

    Created the Employees and Department

---
+++ social-media-spring-main/src/main/java/com/revature/models/Employees.java
@@ -0,0 +1,37 @@
+package com.revature.models;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "employees")
+public class Employees {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "author")
+    private User author;
+
+    @ManyToOne(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinColumn(name = "departmentId")
+    private Department department;
+
+    @Column(name = "date", updatable = false, insertable = false)
+    private Timestamp date;
+
+    public Employees() {
+        this.date = new Timestamp(System.currentTimeMillis());
+    }
+
+    // getters and setters
+}
+
