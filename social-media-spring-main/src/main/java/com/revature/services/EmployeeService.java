e1e69445be55052bb00ace0225064f428901b857
Mason <Mason@DESKTOP-NHDIDLB>
2023-01-31 15:45:17 -0500

    Corrected my spelling mistake

--- social-media-spring-main/src/main/java/com/revature/services/EmployeeService.java
+++ social-media-spring-main/src/main/java/com/revature/services/EmployeeService.java
@@ -1,7 +1,9 @@
 package com.revature.services;
 
 import com.revature.models.Employee;
+import com.revature.models.User;
 import com.revature.repositories.EmployeeRepository;
+import com.revature.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +13,7 @@ import java.util.List;
 public class EmployeeService {
 
     private EmployeeRepository employeeRepository;
+    private UserRepository userRepository;
 
     @Autowired
     public EmployeeService(EmployeeRepository employeeRepository) {
@@ -22,9 +25,15 @@ public class EmployeeService {
         return employeeRepository.findById(empId).orElse(null);
     }
 
-    public List<Employee> getAllEmpoloyees(){
+    public List<Employee> getAllEmployees(){
         List<Employee> emp = employeeRepository.findAll();
         return emp;
+    }
+
+    public Employee createEmployee(int userId, Employee empRepo){
+        User user = userRepository.findById(userId).orElse(null);
+        Employee newEmployee = new Employee(empRepo.getFirstName(), empRepo.getLastName(), user, empRepo.getDepartment());
+        return newEmployee;
     }
 
 
