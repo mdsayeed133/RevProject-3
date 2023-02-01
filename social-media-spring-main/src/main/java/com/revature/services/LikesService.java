26a633edd6fcbf39c13be44577c7d62d6506ed4e
Lightclaw55 <danakhnoukh@yahoo.com>
2023-02-01 16:30:55 -0500

    Started and maybe finished LikesService

--- social-media-spring-main/src/main/java/com/revature/services/LikesService.java
+++ social-media-spring-main/src/main/java/com/revature/services/LikesService.java
@@ -2,6 +2,7 @@ package com.revature.services;
 
 import com.revature.dtos.LikesDTO;
 import com.revature.models.Post;
+import com.revature.models.User;
 import com.revature.repositories.LikesRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,17 +12,35 @@ public class LikesService {
 
     private LikesRepository likesRepo;
     private PostService postService;
+    private UserService userService;
+
 
 
     @Autowired
-    public LikesService(LikesRepository likesRepo) {
+    public LikesService(LikesRepository likesRepo, PostService postService, UserService userService) {
         this.likesRepo = likesRepo;
+        this.postService = postService;
+        this.userService = userService;
     }
 
-    public void likePost(LikesDTO lDTO)
+    public boolean likePost(LikesDTO lDTO)
     {
-        Post post = postService.
+        Post post = postService.getPostById(lDTO.getPostId());
+        User user = userService.getUserById(lDTO.getUserId()).orElse(null);
+
+        if(user != null && post != null)
+        {
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
+    public int likesAmount(int postId)
+    {
+        Post searchPost = postService.getPostById(postId);
+        return likesRepo.countLikes(searchPost);
+    }
 
 }
