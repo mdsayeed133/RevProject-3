ca976cd36f8a4dcbc1b148e39eb476f79efd5a97
Zackery Perski <zackeryperski@gmail.com>
2023-02-06 15:13:40 -0500

    Fixed small discrepencies in test data.

--- python-sql-generator/SocialMediaPopulator/p3modules/generatedSQL.sql
+++ python-sql-generator/SocialMediaPopulator/p3modules/generatedSQL.sql
@@ -1,5 +1,5 @@
 /*Departments Definition Table*/
-insert into departments(title) values('Accounting Team'),('Center of Excellence'),('HR Team'),('IT Support'),('Legal Team'),('Management'),('Marketing Team'),('PDP Team'),('QC Team'),('Recruitment Department'),('Sales Department');
+insert into departments(title) values('Accounting Team'),('Center of Excellence'),('HR Team'),('IT Support'),('Legal Team'),('Management'),('Marketing Team'),('PDP Team'),('QC Team'),('Recruitment Department'),('Sales Department'),('Training Department');
 
 /*Tags Definition Table*/
 insert into tag(tag_name) values('aggravating'),('amusing'),('anxious'),('bad'),('boring'),('challenging'),('childish'),('confusing'),('discouraging'),('disliked'),('engaging'),('friendly'),('frustrating'),('fulfilling'),('glhf'),('good'),('inconsiderate'),('infuriating'),('musical'),('nerd'),('offputting'),('rage-inducing'),('respected'),('respectful'),('stressful'),('understanding');
