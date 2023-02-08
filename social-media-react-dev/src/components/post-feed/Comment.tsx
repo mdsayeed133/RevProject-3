5a7158293b84af061c6dd5474a6d5ae49989be7d
Zackery Perski <zackeryperski@gmail.com>
2023-02-08 17:54:25 -0500

    Updated part of the system. Needs some fixes.

--- social-media-react-dev/src/components/post-feed/Comment.tsx
+++ social-media-react-dev/src/components/post-feed/Comment.tsx
@@ -30,27 +30,16 @@ const Comment: React.FC<any> = (props: {post:RatingPost}) => {
     function thereCanOnlyBeOne()
     {
         let allImageElements = document.getElementsByClassName("onlyOne");
-        for (let i = 0; i<7; i++)
+        for (let i = 0; i<allImageElements.length; i++)
         {
-            allImageElements[i].classList.add("d-none");
-        }
-        switch(ratingPost.imageId)
-        {
-            case 1: allImageElements[0].classList.remove("d-none");
-                    break;
-            case 2: allImageElements[1].classList.remove("d-none");
-                    break;
-            case 3: allImageElements[2].classList.remove("d-none");
-                    break;
-            case 4: allImageElements[3].classList.remove("d-none");
-                    break;
-            case 5: allImageElements[4].classList.remove("d-none");
-                    break;
-            case 6: allImageElements[5].classList.remove("d-none");
-                    break;
-            case 7: allImageElements[6].classList.remove("d-none");
-                    break;
-            default: ;
+            if(!allImageElements[i].classList.contains("ignore"))
+            {   
+                if(allImageElements[i].id != ""+ratingPost.imageId)
+                {
+                    allImageElements[i].classList.add("d-none");
+                }
+                allImageElements[i].classList.add("ignore");
+            }
         }
     }
 
@@ -58,18 +47,19 @@ const Comment: React.FC<any> = (props: {post:RatingPost}) => {
 
     return (
         <>
+        {thereCanOnlyBeOne()}
             <div className="post-card">
                 {/* example card */}
                 <div className="post-message-body">
                     <div className="post-message d-flex justify-content-between">
                         <p>Posted by: <em>{ratingPost.author.email}</em></p>
-                        <img src={one} alt="Sick Reaction" id="one" className="onlyOne"/>
-                        <img src={two} alt="Angry Reaction" id="two" className="onlyOne"/>
-                        <img src={three} alt="Dizzy Reaction" id="three" className="onlyOne"/>
-                        <img src={four} alt="Neutral Reaction" id="four" className="onlyOne"/>
-                        <img src={five} alt="Happy Reaction" id="five" className="onlyOne"/>
-                        <img src={six} alt="Silly Reaction" id="six" className="onlyOne"/>
-                        <img src={seven} alt="Cool Reaction" id="seven" className="onlyOne"/>
+                        <img src={one} alt="Sick Reaction" id="1" className="onlyOne"/>
+                        <img src={two} alt="Angry Reaction" id="2" className="onlyOne"/>
+                        <img src={three} alt="Dizzy Reaction" id="3" className="onlyOne"/>
+                        <img src={four} alt="Neutral Reaction" id="4" className="onlyOne"/>
+                        <img src={five} alt="Happy Reaction" id="5" className="onlyOne"/>
+                        <img src={six} alt="Silly Reaction" id="6" className="onlyOne"/>
+                        <img src={seven} alt="Cool Reaction" id="7" className="onlyOne"/>
                     </div>
                     {/* working code */}
                     <p className="message">{ratingPost.message}</p>
